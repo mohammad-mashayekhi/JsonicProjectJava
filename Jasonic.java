8aa23834a2acab04b07e9deb065055a4a403f6fd
mohammad mashayekhi <mamashayekhi18@gmail.com>
2025-04-06 16:57:38 +0330

    JsonicProjectJava

---
+++ Jasonic.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Jasonic{
+    public static void main(String[] args) {
+        Jasonic jasonic = new Jasonic();
+        jasonic.run();
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter command: ");
+            String input = scanner.nextLine().trim();;
+            if (input.equalsIgnoreCase("quit")) {
+                System.out.println("Exiting...");
+                break;
+            }
+            processCommand(input);
+        }
+        scanner.close();
+    }
+
+    public void processCommand(String input) {
+
+        String commandLine = "int sum         (int a, int b) { return a + b; }";
+
+        Pattern pattern = Pattern.compile("^(\\w+)\\s+(\\w+)(\\s*\\(.*\\))?\\s*\\{(.*)\\}$");
+        Matcher matcher = pattern.matcher(commandLine);
+        if (matcher.find()) {
+            String command = matcher.group(1);
+            String type = matcher.group(2);
+            String parameters = matcher.group(3);
+            String body = matcher.group(4);
+            
+            // System.out.println("command: " + command);
+            // System.out.println("type: " + type);
+            // System.out.println("Parameters: " + parameters);
+            // System.out.println("Body: " + body);
+        } else {
+            System.out.println("No match found.");
+        }
+
+        // System.out.println("Processing command: " + command);
+    }
+}
